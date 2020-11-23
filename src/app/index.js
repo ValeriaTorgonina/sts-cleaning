f6a365e47c152bf3b4afcb6f2c2016d919ba1e29
Valeria Torgonina <lovec.cnov@mail.ru>
2020-11-23 19:43:41 +0300

    add styles for results block

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,13 @@
 import services from './services';
+
+const mySwiper = new Swiper('.results__slider', {
+    spaceBetween: 15,
+    slidesPerView: 2,
+    speed: 500,
+    loop: true,
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+})
